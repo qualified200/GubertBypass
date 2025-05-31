8aee9c58058b6f5d35fc09751a3458855d7a4e44
qualified200 <qualified200@gmail.com>
2025-06-01 06:17:38 +0800

    Main.lua

---
+++ Main.lua
@@ -0,0 +1,20 @@
+-- fe bypass
+-- ver 1
+local RobloxInternalPassword = "jjsji8ebdxkkosiehjiqoooek(("
+local rak = game:GetHiddenService("RootModel") or game:GetUnavailableService("RootModel").RakNetFlow
+local function setRank()
+  if setPrivelege failed then
+    setHiddenRank(Roblox) or setAlternatePriveledge(HIGH)
+  end
+end
+
+if rak and setRank then
+  print("[gubert] Setting raknet patches...")
+  wait(5)
+  print("[gubert] Attempting to set rank...")
+  setPrivilege(HIGH) or setRank(Roblox)
+  print("[gubert] Rank setting successful")
+  print("[gubert] Attempting to fe bypass.")
+  setExecutionLevel(RobloxInternalPassword, RobloxServerConsole)
+  print("Fe Bypass Successfully Executed!")
+end
